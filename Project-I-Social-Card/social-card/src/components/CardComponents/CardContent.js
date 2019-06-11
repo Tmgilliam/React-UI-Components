5fce9609d8664311b3a08f9b36ec4a94887d9971
Tatianna <gilliamtatiannam@gmail.com>
2019-06-11 08:09:10 -0700

    So confused
    Only done one card so far and it has errors.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContent = () => {
+    return ( <
+        div className = 'card-text-content' >
+
+        <
+        /div>
+    );
+}
